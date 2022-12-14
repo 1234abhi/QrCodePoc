8619d76ed7f6dee1a900f48284abe71a9e8dded5
Abhishek Negi <abhishek.negi.9528@gmail.com>
2022-12-14 16:30:22 +0530

    backcameraenable

--- src/QrCode.js
+++ src/QrCode.js
@@ -22,7 +22,7 @@ const QrCode = () => {
       handleStop();
     };
     html5QrCode.start(
-      { facingMode: { exact: "user" } },
+      { facingMode: "environment" },
       qrConfig,
       qrCodeSuccessCallback
     );
